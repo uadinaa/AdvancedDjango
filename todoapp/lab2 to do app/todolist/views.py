bac691c5215adead013874c47068436b371a40a1
Abitova Dina <123065685+uadinaa@users.noreply.github.com>
2025-02-13 23:42:01 +0500

    Add files via upload

---
+++ todoapp/lab2 to do app/todolist/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_http_methods
+from .models import Todo
+
+
+def index(request):
+    todos = Todo.objects.all()
+    return render(request, "base.html", {"todo_list": todos})
+
+
+@require_http_methods(["POST"])
+def add(request):
+    # if request.method == "POST":
+    title = request.POST["title"]
+    todo = Todo(title=title)
+    todo.save()
+    return redirect("index")
+
+
+def update(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.complete = not todo.complete
+    todo.save()
+    return redirect("index")
+
+
+def delete(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.delete()
+    return redirect("index")
+
+
+@require_http_methods(["GET", "POST"])
+def edit(request, todo_id):
+    todo = get_object_or_404(Todo, id=todo_id)
+
+    if request.method == 'POST':
+        todo.title = request.POST["title"]
+        todo.save()
+        return redirect("index")
+
+    return render(request, "edit.html", {"todo": todo})
