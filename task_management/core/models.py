5935bbb5a026c5459544ccfa8929238c6f69d4a5
Abitova Dina <123065685+uadinaa@users.noreply.github.com>
2025-03-17 22:15:17 +0500

    practice1

---
+++ task_management/core/models.py
@@ -0,0 +1,75 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser, Group, Permission
+
+
+# class User(models.Model):
+#     first_name = models.CharField(max_length=50)
+#
+#     last_name = models.CharField(max_length=50)
+#
+#     email = models.EmailField(unique=True)
+#
+#     def __str__(self):
+#         return f"{self.first_name} {self.last_name}"
+
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('admin', 'Admin'),
+        ('manager', 'Manager'),
+        ('employee', 'Employee'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='employee')
+
+    groups = models.ManyToManyField(Group, related_name="custom_user_groups", blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name="custom_user_permissions", blank=True)
+
+    def __str__(self):
+        return f"{self.username} ({self.role})"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100)
+
+    description = models.TextField()
+
+    start_date = models.DateField()
+
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Priority(models.Model):
+    level = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.level
+
+
+class Task(models.Model):
+    # objects = None
+    title = models.CharField(max_length=200)
+
+    description = models.TextField()
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    priority = models.ForeignKey(Priority, on_delete=models.SET_NULL, null=True)
+
+    assignee = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    due_date = models.DateField()
+
+    def __str__(self):
+        return self.title
